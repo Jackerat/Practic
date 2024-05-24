6b12da55fb1000948939935fc9e558e494159783
moonvolk4 <84585564+moonvolk4@users.noreply.github.com>
2024-05-24 15:49:28 +0300

    Update Button.h

--- include/Button.h
+++ include/Button.h
@@ -1,1 +1,55 @@
+#include "Widgets.h"
 
+
+class Button {
+private:
+    sf::RectangleShape shape;
+    sf::Text text;
+    bool isChecked;
+
+public:
+    Button(sf::Vector2f position, sf::Vector2f size, sf::Font& font, std::string buttonText) {
+        shape.setPosition(position);
+        shape.setSize(size);
+        shape.setFillColor(sf::Color::White);
+        shape.setOutlineThickness(2.f);
+        shape.setOutlineColor(sf::Color::Black);
+
+        text.setFont(font);
+        text.setCharacterSize(20);
+        text.setFillColor(sf::Color::Black);
+        text.setPosition(position);
+        text.setString(buttonText);
+
+        isChecked = false;
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(shape);
+        window.draw(text);
+    }
+
+    bool isHovered(sf::Vector2f mousePos) {
+        if (shape.getGlobalBounds().contains(mousePos)) {
+            shape.setFillColor(sf::Color(192,192,192));
+            return true;
+        }
+        else {
+            shape.setFillColor(sf::Color(242,243,244));
+            return false;
+        }
+    }
+
+    bool isClicked(sf::Vector2f mousePos) {
+        if (shape.getGlobalBounds().contains(mousePos)) {
+            isChecked = !isChecked;
+            shape.setFillColor(sf::Color(192,192,192));
+            return true;
+        }
+        return false;
+    }
+
+    bool isCheckedState() {
+        return isChecked;
+    }
+};
